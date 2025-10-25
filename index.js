971b7ffe46104c62092ad06cf8360a126b44931b
Sanket bisne <sanketbisne@gmail.com>
2025-10-25 20:55:20 +0530

    Add placeholder image

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(`
+    <h1>Hello, World!</h1>
+    <img src="https://placehold.co/600x400" alt="placeholder image">
+  `);
 });
 
 app.listen(port, () => {
